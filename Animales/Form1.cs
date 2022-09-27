69a9f7c7af98942c7c8983e04a1944f30942a6ae
CarlosRMagana <92707043+CarlosRMagana@users.noreply.github.com>
2022-09-26 21:42:28 -0600

    prueba

--- Animales/Form1.cs
+++ Animales/Form1.cs
@@ -16,5 +16,10 @@ namespace Animales
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("hola");
+        }
     }
 }
