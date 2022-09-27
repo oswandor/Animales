11957d0c12f3783c07388796d92279049179dc32
oswandor <oswandor26@gmail.com>
2022-09-26 22:03:34 -0600

    propiedades animal

--- Animales/Animales.cs
+++ Animales/Animales.cs
@@ -8,8 +8,14 @@ namespace Animales
 {
     internal class Animales
     {
+        private double tamanio;
+        private double peso;
+        private string genero;
+        private string nombre;
 
-
-
+        public double Tamanio { get => tamanio; set => tamanio = value; }
+        public double Peso { get => peso; set => peso = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
     }
 }
